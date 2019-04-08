66ed11be09b04f947b43cba91a5fd9f2776e06b2
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-04-08 09:45:01 -0300

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/sab'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
-const PORT = 4200;
+const PORT = process.env.PORT || 4200;
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sab/index.html'));
